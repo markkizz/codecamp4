13f28bad3acd194c994fcd2cf3a25f3fe8758788
markkizz <mark.pattanapara@gmail.com>
2019-10-24 10:07:17 +0700

    codekata day2 finish

--- src/day2/main.js
+++ src/day2/main.js
@@ -1,6 +1,17 @@
 <<<<<<< HEAD
 function main(str, str2) {
+<<<<<<< HEAD
 	// Your code begins here;
+=======
+    let isDiff = 0;
+    for(let i=0 ; i<str.length ; i++){
+        // if(str[i] !== str2[i]) {
+        //     isDiff += 1;
+        // }
+        str[i] !== str2[i] ? isDiff++ : null;
+    }
+    return isDiff;
+>>>>>>> codekata day2 finish
 }
 
 module.exports = { main };
